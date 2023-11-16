35058cf2bbd12d01f31307d06ea34e8f80a6c05d
Kuldeep Kushwaha <82083060+Kuldeep-kk@users.noreply.github.com>
2023-11-16 21:41:31 +0530

    PostCard.js
    Updating Transition

--- src/app/Components/Postcard/PostCard.js
+++ src/app/Components/Postcard/PostCard.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import Image from "next/image";
+import { motion } from "framer-motion";
 import like from "../../../../public/ImageSrc/like.png";
 
 
-const PostCard = ({post,author}) => {
+const PostCard = ({post,author,index}) => {
     return (
-        <div className={`p-5 border-b-2`}>
+        <motion.div className={`p-5 border-b-2`}
+
+             initial={{
+                 opacity: 0,
+                 x: index % 2 === 0 ? 50 : -50
+             }}
+             whileInView={{
+                 opacity: 1,
+                 x: 0,
+                 transition: {
+                     duration: 1
+                 }
+             }}
+             viewport={{  }}
+        >
             <div className={`flex justify-between items-center`}>
-                <h2 className={`font-extrabold text-slate-600 text-[16px]`}>{post.title}</h2>
+                <h2 className={`font-extrabold text-slate-600 text-[18px] md:text-[16px]`}>{post.title}</h2>
                 <Image src={like} alt={'Verified Icon'} className={`h-7 w-8 active:scale-75 duration-300 hover:cursor-pointer`} />
             </div>
-            <p className={`mt-3 text-slate-500 md:text-[15px] whitespace-pre-line text-[14px]`}>{post.content}</p>
+            <p className={`mt-3 text-slate-500 md:text-[15px] whitespace-pre-line text-[16px] md:text-[14px]`}>{post.content}</p>
             <div className={`flex justify-between md:text-[12px]  font-semibold mt-6 text-[12px]`}>
                 <div>
                     <span  style={{ color: '#22d3ee' }}>{post.type}</span> by {author.toLocaleLowerCase()}
@@ -20,7 +35,7 @@ const PostCard = ({post,author}) => {
                 </div>
             </div>
 
-        </div>
+        </motion.div>
     );
 };
 
